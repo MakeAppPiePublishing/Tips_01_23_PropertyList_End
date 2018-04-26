34fd3ae446882da0fcbd07890969dd8187ed64d9
Steve Lipton <stevelipton@brk-16892.local>
2018-04-26 05:21:08 -0700

    Initial Commit

---
+++ PropertyList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PropertyList
+//
+//  Created by Steve Lipton on 4/26/18.
+//  Copyright © 2018 Steve Lipton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
